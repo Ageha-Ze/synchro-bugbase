47deac59802e38c13714bd94412ad720b5edf554
Agha <yougotthethunder@gmail.com>
2025-11-12 06:29:46 +0700

    Integrate SpeedInsights into RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,19 @@
 // app/layout.tsx
 import './globals.css';
 import { ToastProvider } from "@/components/ui/use-toast";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="pb-20 md:pb-0 bg-gray-50">
+      <body>
         <ToastProvider>
           {children}
+          <SpeedInsights 
+          debug={process.env.NODE_ENV === 'development'}
+          sampleRate={1}
+          />
         </ToastProvider>
       </body>
     </html>
