70d346fda70fb5728e2bd2ef81c06900564aadfb
Agha <yougotthethunder@gmail.com>
2025-11-11 08:32:57 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 // app/layout.tsx
 import './globals.css';
-
 import { ToastProvider } from "@/components/ui/use-toast";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className="pb-20 md:pb-0 bg-gray-50">
         <ToastProvider>
           {children}
         </ToastProvider>
