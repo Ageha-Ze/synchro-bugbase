b3bf262b19c583c4091091eb23fd26e9ffcc762c
Agha <yougotthethunder@gmail.com>
2025-11-14 08:41:25 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,19 +3,18 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { ToastProvider } from "@/components/ui/use-toast";
 
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         <ThemeProvider
-          attribute="class"         // <html class="dark"> → Tailwind dark mode aktif
-          defaultTheme="system"     // ikut tema OS
-          enableSystem={true}       // perbolehkan deteksi sistem
+          attribute="class"
+          defaultTheme="system"
+          enableSystem={true}
         >
-        <ToastProvider>
-          {children}
-        </ToastProvider>
+          <ToastProvider>
+            {children}
+          </ToastProvider>
         </ThemeProvider>
       </body>
     </html>
